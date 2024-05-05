5e791593f5961e7fd652ee8fad2bf83d69de0651
prathamlkamath <134483132+prathamlkamath@users.noreply.github.com>
2024-05-05 12:06:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+
+let products = [
+    {
+        id: 1,
+        name: 'Nike T Shirt',
+        image: 'shirt1.png',
+        price: 2000
+    },
+    {
+        id: 2,
+        name: 'Nike Pro',
+        image: '2.PNG',
+        price: 2987
+    },
+    {
+        id: 3,
+        name: 'Nike Dri-FIT Icon',
+        image: '3.PNG',
+        price: 2885
+    },
+    {
+        id: 4,
+        name: 'Nike SB',
+        image: '4.PNG',
+        price: 3500
+    },
+    {
+        id: 5,
+        name: 'NikeCourt Dri-FIT',
+        image: '5.PNG',
+        price: 3255
+    },
+    {
+        id: 6,
+        name: 'Paris Saint-Germain',
+        image: '6.PNG',
+        price: 4500
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
