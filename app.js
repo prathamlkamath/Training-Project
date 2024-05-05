e8bbb9c920b523a231a0434732ad3a9a5cb4b535
prathamlkamath <134483132+prathamlkamath@users.noreply.github.com>
2024-05-05 12:13:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function initApp(){
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
-            <img src="image/${value.image}">
+            <img src="${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Add To Cart</button>`;
