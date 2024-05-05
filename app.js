1e8ced2fc6946dec207adc5aac1146217575ca20
prathamlkamath <134483132+prathamlkamath@users.noreply.github.com>
2024-05-05 12:19:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ function initApp(){
             <img src="${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">Add To Cart</button>`;
+            <button onclick="">Add To Cart</button>`;
         list.appendChild(newDiv);
     })
 }
